ac5ac732b93129c989bbf272340706929ddfef95
fjatoi <Fahar_Jatoi@intuit.com>
2017-08-11 15:33:20 -0700

    index.js with constructor

--- build/index.js
+++ build/index.js
@@ -83,6 +83,7 @@ module.exports = require("react");
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+exports.HelloReactComponent = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -98,16 +99,16 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Hello = function (_React$Component) {
-    _inherits(Hello, _React$Component);
+var HelloReactComponent = exports.HelloReactComponent = function (_React$Component) {
+    _inherits(HelloReactComponent, _React$Component);
 
-    function Hello() {
-        _classCallCheck(this, Hello);
+    function HelloReactComponent() {
+        _classCallCheck(this, HelloReactComponent);
 
-        return _possibleConstructorReturn(this, (Hello.__proto__ || Object.getPrototypeOf(Hello)).apply(this, arguments));
+        return _possibleConstructorReturn(this, (HelloReactComponent.__proto__ || Object.getPrototypeOf(HelloReactComponent)).apply(this, arguments));
     }
 
-    _createClass(Hello, [{
+    _createClass(HelloReactComponent, [{
         key: 'render',
         value: function render() {
             return _react2.default.createElement(
@@ -118,8 +119,14 @@ var Hello = function (_React$Component) {
         }
     }]);
 
-    return Hello;
+    return HelloReactComponent;
 }(_react2.default.Component);
+
+var Hello = function Hello(elem) {
+    _classCallCheck(this, Hello);
+
+    ReactDOM.render(_react2.default.createElement(HelloReactComponent, null), elem);
+};
 
 exports.default = Hello;
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-class Hello extends React.Component {
+export class HelloReactComponent extends React.Component {
     render() {
         return (
             <div>This is Hello!</div>
     );
     }
 }
-export default Hello;
+export default class Hello{
+    constructor(elem){
+        ReactDOM.render(  <HelloReactComponent />, elem)
+    }
+}
